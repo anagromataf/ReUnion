b410ba4b51d44ecbe812a68a66997801c7bfc3fb
Tobias Kräntzer <info@tobias-kraentzer.de>
2014-03-26 22:38:24 +0100

    Initial Commit

---
+++ ReUnion/ReUnion/RUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RUAppDelegate.h
+//  ReUnion
+//
+//  Created by Tobias Kräntzer on 26.03.14.
+//  Copyright (c) 2014 Tobias Kräntzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ReUnion/ReUnion/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ReUnion
+//
+//  Created by Tobias Kräntzer on 26.03.14.
+//  Copyright (c) 2014 Tobias Kräntzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RUAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RUAppDelegate class]));
+    }
+}
